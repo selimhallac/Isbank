83d2e9c55b423f9f0ba48f24a112c85b1879bbe8
Selim Hallaç <selimhallac@gmail.com>
2023-02-22 16:41:26 +0300

    Initial commit

---
+++ src/Isbank.class.php
@@ -0,0 +1,61 @@
+<?php
+
+Class Isbank
+{
+    
+    
+    public $username = "";
+    public $pasword = "";
+    
+    function __construct($username, $password)
+    {
+        $this->username       = $username;
+        $this->password       = $password;
+    }
+    
+    
+    public function hesap_hareketleri()
+    {
+        
+        try {
+
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://posmatik2.isbank.com.tr/Authenticate.aspx",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS => array('uid' => $this->username,'pwd' => $this->password),
+            ));
+        
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            curl_close($curl);
+        
+            @$objectarray =simplexml_load_string($response);
+            // İşlem başarılı
+            if ( isset($objectarray->Tarih)){
+                    $res['statu'] = true;
+                    $res['response'] = $objectarray;
+                    echo json_encode($res);
+        
+            } else {
+                $res['statu'] = false;
+                $res['response'] = $response;
+                echo json_encode($res);
+            }
+            // İşlem başarılı
+        
+        } catch (Throwable $e) {
+            $res['statu'] = false;
+            $res['response'] = 'Bağlantı problemi oluştu.';
+            echo json_encode($res);
+        }
+
+    }
+    
+    
+}
